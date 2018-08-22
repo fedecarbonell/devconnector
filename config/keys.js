7b5f435d57358c3f024d58ed2a9021ed55ef629b
federcarbonell <fedecarbonell@gmail.com>
2018-08-22 20:56:50 -0300

    clase 11, login y JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://federic0:federic0@ds147265.mlab.com:47265/devconnector"
+  mongoURI: "mongodb://federic0:federic0@ds147265.mlab.com:47265/devconnector",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // @route   GET api/users/test
 // @desc    tests users route
@@ -44,6 +46,46 @@ router.post("/register", (req, res) => {
         });
       }
     });
+});
+
+// @route   GET api/users/login
+// @desc    Login user / rerurning the JWT token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find the user by email
+  User.findOne({ email }).then(user => {
+    // Check user
+    if (!user) {
+      return res.status(404).json({ email: "User not foud" });
+    }
+
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //User matched
+
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+        //Sign the token
+        JWT.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
 });
 
 module.exports = router;
